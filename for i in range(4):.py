87aee17bff711105d29b40dfa86fab5896f95324
amaliabattle <amalia@astranova.org>
2021-10-15 14:19:17 -0600

    new file for basics of python code

---
+++ for i in range(4):.py
@@ -0,0 +1,20 @@
+a = 8
+print(type(a))
+
+is_water_wet = True  
+print(is_water_wet)
+
+annoy_viewer = "supercalifragilisticexpialidocious"
+print (annoy_viewer * 150)
+
+for i in range(56):
+        print(str(i)) + ": hello world"
+
+numbers_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+print(numbers_list[0])
+
+goose_is_loud = True
+if goose_is_loud:
+    print("goose is loud!!!!")
+else:
+    print("goose is not loud")
